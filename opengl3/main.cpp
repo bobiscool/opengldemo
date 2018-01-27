99dc52d916ac813909813f6fcbd3163479a75ab9
bobiscool <tomisugly@163.com>
2018-01-27 11:07:58 +0800

    Initial Commit

---
+++ opengl3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl3
+//
+//  Created by mac on 18/1/27.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
