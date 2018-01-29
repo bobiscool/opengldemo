6f6635eb4335a0bc2d153eb8ef1cdfb1901655a8
bobiscool <tomisugly@163.com>
2018-01-29 10:50:35 +0800

    [Fix] 擦....终于可以写 open gl 了/...

--- opengl3/main.cpp
+++ opengl3/main.cpp
@@ -5,11 +5,42 @@
 //  Created by mac on 18/1/27.
 //  Copyright © 2018年 mac. All rights reserved.
 //
+#include "iostream"
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
 
-#include <iostream>
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        
+        glfwPollEvents();  //Needed, or you will get a spinning beach ball
+    }
+    
+    glfwDestroyWindow(window);
+    
+    glfwTerminate();
+    
 }
