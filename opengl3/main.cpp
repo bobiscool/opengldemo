4338dda618b43d39605da8acf33be2596a5daddd
bobiscool <tomisugly@163.com>
2018-01-31 17:50:55 +0800

    [Chg] 修改 着色器代码

--- opengl3/main.cpp
+++ opengl3/main.cpp
@@ -21,7 +21,7 @@ const GLuint WIDTH = 800,HEIGHT = 600;
 
 // Shaders
 const GLchar* vertexShaderSource = "#version 330 core\n"
-"layout (location=0) in cec3 position;\n"
+"layout (location=0) in vec3 position;\n"
 "void main()\n"
 "{\n"
 "gl_Position = vec4(position.x,position.y,position.z,1.0);\n"
@@ -33,9 +33,10 @@ const GLchar* fragmentShaderSource = "#version 330 core\n"
 "out vec4 color;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"color = vec4(1.0f,0.5f,0.2f,1.0f);\n"
 "}\n\0"
 ;
+// 着色器 书写 出了点问题
 
 int main(){
     // init glfw
@@ -45,6 +46,7 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
     
     // 创建一个 窗口 对象
