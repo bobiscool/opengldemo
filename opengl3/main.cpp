f311dcf9ed3c445d57fbd25155d3c09c880c4f1d
bobiscool <tomisugly@163.com>
2018-01-31 17:34:10 +0800

    [Chg] 自己敲一遍 三角形

--- opengl3/main.cpp
+++ opengl3/main.cpp
@@ -5,152 +5,170 @@
 //  Created by mac on 18/1/27.
 //  Copyright © 2018年 mac. All rights reserved.
 //
-#include "iostream"
+#include <iostream>
+
+//GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
-// GLFW
+
 #include <GLFW/glfw3.h>
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback(GLFWwindow* window,int key,int scancode,int action,int mode);
 
-// Window dimensions
-const GLuint WIDTH = 800, HEIGHT = 600;
+
+const GLuint WIDTH = 800,HEIGHT = 600;
+
 
 // Shaders
 const GLchar* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 position;\n"
+"layout (location=0) in cec3 position;\n"
 "void main()\n"
 "{\n"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-"}\0";
+"gl_Position = vec4(position.x,position.y,position.z,1.0);\n"
+"}\0"
+;
+
+
 const GLchar* fragmentShaderSource = "#version 330 core\n"
 "out vec4 color;\n"
 "void main()\n"
 "{\n"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
+"gl_Position = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"}\n\0"
+;
 
-
-int main()
-{
+int main(){
+    // init glfw
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
+    // 设置窗口 属性
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
     
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    // 创建一个 窗口 对象
+    GLFWwindow* window = glfwCreateWindow(WIDTH,HEIGHT,"learnOpenGL",nullptr,nullptr);
+    //
     glfwMakeContextCurrent(window);
+    // 设置回调
+    glfwSetKeyCallback(window,key_callback);
     
-    // Set the required callback functions
-    glfwSetKeyCallback(window, key_callback);
-    
-    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    // 下面是干啥的 还不知道
     glewExperimental = GL_TRUE;
-    // Initialize GLEW to setup the OpenGL Function pointers
+    
     glewInit();
     
-    // Define the viewport dimensions
-    int width, height;
+    
+    // 设置 视窗 尺寸
+    int width,height;
     glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
+    glViewport(0,0,width,height);
     
     
-    // Build and compile our shader program
-    // Vertex shader
+    //顶点着色器
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
     glCompileShader(vertexShader);
-    // Check for compile time errors
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    
+    //
+    GLint success; // 检测是否编译成功
+    GLchar infoLog[512];//
+    
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);//& 在函数传值里面 是什么意思  是不是 创建一个值 然后 在函数里面 去取值
+    
+    if(!success){
+        glGetShaderInfoLog(vertexShader,512,NULL,infoLog);
+        std::cout << "ERROR::SHZDER::VERTEX::COMPILATION_FAILED\n"<<infoLog<<std::endl;
     }
-    // Fragment shader
+    
+    // Fragement shader 片元着色器
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
     glCompileShader(fragmentShader);
-    // Check for compile time errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    
+    // 检测编译错误
+    
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS,&success);
+    if(!success){
+        glGetShaderInfoLog(fragmentShader,512,NULL,infoLog);
+        std::cout <<"ERROR::SHADER::FRAGEMENT::COMPILATION_FAILED\n"<<infoLog<<std::endl;
     }
-    // Link shaders
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
+    
+    
+    // 链接 着色器
+    GLuint shaderProgram = glCreateProgram(); // 创建着色器 程序
+    glAttachShader(shaderProgram,vertexShader); // 将着色器 附着在 着色程序
+    glAttachShader(shaderProgram,fragmentShader);
     glLinkProgram(shaderProgram);
-    // Check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    
+    // 是否 link 错误
+    glGetProgramiv(shaderProgram,GL_LINK_STATUS,&success);
+    if(!success){
+        glGetProgramInfoLog(shaderProgram,512,NULL,infoLog);
+        std::cout <<"ERROR::SHADER::PROGRAM::LINKING_FAILED\n"<<infoLog<<std::endl;
     }
-    glDeleteShader(vertexShader);
+    
     glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
     
-    
-    // Set up vertex data (and buffer(s)) and attribute pointers
-    GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f, // Left
-        0.5f, -0.5f, 0.0f, // Right
-        0.0f,  0.5f, 0.0f  // Top
+    // 设置完 着色器  开始设置 绘制程序
+    GLfloat vertices[]={
+        -0.5f,-0.5f,0.0f,
+         0.5f,-0.5f,0.0f,
+         0.5f, 0.5f,0.0f
     };
-    GLuint VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    
+    GLuint VBO,VAO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1,&VBO);
+    // 绑定一堆东西
     glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
     
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat),(GLvoid*)0);
     glEnableVertexAttribArray(0);
     
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
     
-    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+    glBindBuffer(GL_ARRAY_BUFFER,0);
     
-    // Game loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+    glBindVertexArray(0);
+    
+    
+    while(!glfwWindowShouldClose(window)){
+        // 研究事件
         glfwPollEvents();
         
-        // Render
-        // Clear the colorbuffer
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // 清除 颜色
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        // Draw our first triangle
+        // 绘制 第一个三角形
+        
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES,0,3);
         glBindVertexArray(0);
-        
-        // Swap the screen buffers
+        //
         glfwSwapBuffers(window);
     }
-    // Properly de-allocate all resources once they've outlived their purpose
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    // Terminate GLFW, clearing any resources allocated by GLFW.
-    glfwTerminate();
-    //return 0;
     
+    glDeleteVertexArrays(1,&VAO);
+    glDeleteBuffers(1,&VBO);
+    
+    //
+    glfwTerminate();
+    
+    return 0;
 }
 
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+void key_callback(GLFWwindow* window,int key,int scancode,int action,int mode){
+    if (key == GLFW_KEY_ESCAPE&&action ==GLFW_PRESS) {
+        glfwSetWindowShouldClose(window,GL_TRUE);
+    }
 }
+
+
+
